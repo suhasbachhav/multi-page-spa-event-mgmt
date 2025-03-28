f1f78b83f26623639976c6f01a8fb7cdf05f95ae
Suhas Bachhav <ksb.suhas@gmail.com>
2025-03-28 23:51:13 +0530

    defer

--- frontend/src/pages/Error.jsx
+++ frontend/src/pages/Error.jsx
@@ -1,29 +1,31 @@
-import { useRouteError } from "react-router-dom";
-import PageContent from "../components/PageContent";
-import MainNavigation from "../components/MainNavigation";
+import { useRouteError } from 'react-router-dom';
+import MainNavigation from '../components/MainNavigation';
+
+import PageContent from '../components/PageContent';
 
 function ErrorPage() {
-    const error = useRouteError();
+  const error = useRouteError();
 
-    let title = "An error Occured";
-    let message = "Something went wrong";
+  let title = 'An error occurred!';
+  let message = 'Something went wrong!';
 
-    if(error.status === 500){
-        message = JSON.parse(error.data).message;
-    }
+  if (error.status === 500) {
+    message = error.data.message;
+  }
 
-    if(error.status === 404){
-        title = 'Not Found';
-        message = 'Could not find the results';
-    }
-    return ( 
-        <>
-            <MainNavigation />
-            <PageContent title={title}>
-                <p>{message}</p>
-            </PageContent>
-        </>
-     );
+  if (error.status === 404) {
+    title = 'Not found!';
+    message = 'Could not find resource or page.';
+  }
+
+  return (
+    <>
+      <MainNavigation />
+      <PageContent title={title}>
+        <p>{message}</p>
+      </PageContent>
+    </>
+  );
 }
 
 export default ErrorPage;
