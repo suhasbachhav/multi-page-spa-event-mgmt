4bfd68465194d3316ed8b23c9c1117d18dc06714
Suhas Bachhav <ksb.suhas@gmail.com>
2025-03-29 11:52:40 +0530

    route protection

--- frontend/src/util/auth.js
+++ frontend/src/util/auth.js
@@ -8,11 +8,15 @@ export function getAuthToken(){
 
 
 export function tokenLoader(){
+    return getAuthToken();
+}
+
+export function checkAuthLoader() {
     const token = getAuthToken();
-  
+    
     if (!token) {
       return redirect('/auth');
     }
    
     return null;
-}
+  }
