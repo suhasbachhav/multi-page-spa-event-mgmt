4e84f1617b95560f1158e8acf15e365b0768a631
Suhas Bachhav <ksb.suhas@gmail.com>
2025-03-29 12:11:31 +0530

    logout and token expiry

--- frontend/src/util/auth.js
+++ frontend/src/util/auth.js
@@ -3,9 +3,28 @@ import { redirect } from "react-router-dom";
 export function getAuthToken(){
     const token = localStorage.getItem('token');
 
+    if (!token) {
+        return null;
+      }
+
+    const tokenDuration = getTokenDuration();
+
+    if(tokenDuration < 0){
+        return "EXPIRED";
+    }
+
     return token;
 }
 
+export function getTokenDuration(){
+    const storedExpiration = localStorage.getItem('expiration');
+
+    const expirationDate = new Date(storedExpiration);
+    const now = new Date();
+    const duration = expirationDate.getTime() - now.getTime();
+
+    return duration;
+}
 
 export function tokenLoader(){
     return getAuthToken();
